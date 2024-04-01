3fb352fe1461bcf994c3847c23dd7982d6e1b60c
Hieunmtse150772 <nguyenminhtrunghieu10@gmail.com>
2024-04-01 11:20:35 +0700

    fix localstorage is not defined

--- AnNhien_FE/app/components/ModeToggle/index.tsx
+++ AnNhien_FE/app/components/ModeToggle/index.tsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from 'react';
 
 
 export function ModeToggle() {
-  const currentMode = localStorage.getItem('mode');
+  let currentMode = 'light';
+  if (typeof window !== 'undefined') { localStorage.getItem('mode'); }
   const [mode, setMode] = useState<boolean>(currentMode === 'light' ? false : true);
   const handleClickDarkMode = () => {
     setMode(!mode)
@@ -17,12 +18,10 @@ export function ModeToggle() {
     if (mode) {
       document.documentElement.classList.add('dark')
       document.documentElement.classList.add('dark-mode-navbar')
-
     } else {
       document.documentElement.classList.remove('dark')
       document.documentElement.classList.remove('dark-mode-navbar')
     }
-
   }, [mode])
   return (
     <IconButton className='toggle_button' sx={{ ml: 1, color: 'white' }} onClick={handleClickDarkMode} color="inherit">

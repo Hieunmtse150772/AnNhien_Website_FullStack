32bf77a8b073fe92b655ae418cbf2d3c186ecee7
Hieunmtse150772 <nguyenminhtrunghieu10@gmail.com>
2024-04-29 22:15:57 +0700

    [FE] store

--- AnNhien_FE/app/components/Store/index.tsx
+++ AnNhien_FE/app/components/Store/index.tsx
@@ -6,88 +6,69 @@ export default function Store({
 }) {
     const news = [
         {
-            name: 'Đậu hủ chiên',
-            src: '/assets/dishvegetable/vegetable_1.jpeg'
+            name: '2foods-Gâteau Chocolate, 16oz',
+            src: '/assets/food/8TrackFoods-OrganicWhiteCannelliniBeans_15oz_200x200.avif'
         },
         {
-            name: 'Ớt chuông xào',
-            src: '/assets/dishvegetable/vegetable_2.jpeg'
+            name: '8 Track Foods - Organic White Cannellini Beans, 15 oz',
+            src: '/assets/food/57AB2533-640B-48F4-A09C-FB60800D7CC2_200x200.avif'
         },
         {
-            name: 'Dưa leo',
-            src: '/assets/dishvegetable/vegetable_4.jpeg'
+            name: 'Abbot`s Butcher - Plant-Based Meats, 10oz | Multiple',
+            src: '/assets/food/Abbot_sButcher-Plant-BasedMeats_10ozGroundBeef_200x200.avif'
         },
         {
-            name: 'Dưa leo',
-            src: '/assets/dishvegetable/vegetable_4.jpeg'
+            name: 'Ancient Harvest - Traditional Quinoa, 27oz',
+            src: '/assets/food/Abbot_sButcher-Plant-BasedMeats_10ozGroundBeef_200x200.avif'
         },
         {
-            name: 'Dưa leo',
-            src: '/assets/dishvegetable/vegetable_4.jpeg'
-        },
-        {
-            name: 'Dưa leo',
-            src: '/assets/dishvegetable/vegetable_4.jpeg'
+            name: 'Anima Mundi - Blue Lotus Tea: Flower of Intuition, 1oz',
+            src: '/assets/food/AncientHarvest-TraditionalQuinoa_27oz_200x200.avif'
         },
         {
             name: 'Dưa leo',
-            src: '/assets/dishvegetable/vegetable_4.jpeg'
+            src: '/assets/food/AnimaMundi-BlueLotusTeaFlowerofIntuition_1oz_200x200.avif'
+        },
+        {
+            name: 'Dưa leo',
+            src: '/assets/food/AncientHarvest-TraditionalQuinoa_27oz_200x200.avif'
         },
     ]
     return (
         <>
-            <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-5">
+            <div className="m-10 grid gap-10  xs:grid-cols-4 md:grid-cols-3 lg:gap-10 xl:grid-cols-5">
                 {news.map((item, index) => (
                     <div key={index}
-                        className="group cursor-pointer gap-10 md:grid-cols-2 mt-10"            >
+                        className="overflow-hidden bg-gray-100 transition-all hover:scale-105 group cursor-pointer gap-10 md:grid-cols-2 mt-10 rounded-xl shadow-md hover:shadow-black w-full h-full">
                         <div
-                            className="overflow-hidden rounded-md bg-gray-100 transition-all hover:scale-105 dark:bg-gray-800"                >
+                            className="dark:bg-gray-800">
                             <Link
                                 className="relative block   aspect-video"
-
                                 href="">
                                 <Image
-                                    src="/assets/banner/background.png"
+                                    src={item.src}
                                     alt={"Thumbnail"}
                                     priority={true}
                                     className="object-cover transition-all"
-                                    fill
-                                    sizes="(max-width: 768px) 30vw, 33vw"
+                                    width={500}
+                                    height={500}
                                 />
                             </Link>
                         </div>
 
-                        <div className="flex items-center">
+                        <div className="flex items-center w-full p-2  justify-center">
                             <div>
-                                <div className="flex gap-3">
+                                <div className="flex gap-3 ">
                                     <Link
                                         href={`/category/`}
                                     >
                                         <span
-                                            className="inline-block text-xs font-medium tracking-wider uppercase mt-5"
+                                            className=" content-center"
                                         >
-                                            {'Loại bài viết'}
+                                            {item.name}
                                         </span>
                                     </Link>
                                 </div>
-                                <h2
-                                    className="text-3xl line-clamp-2 font-medium  tracking-normal text-black"
-                                >
-                                    <Link
-                                        href={""}>
-                                        <span
-                                            className="bg-gradient-to-r from-green-200 to-green-100 bg-[length:0px_10px] bg-left-bottom
-                                           bg-no-repeat
-                                           transition-[background-size]
-                                           duration-500
-                                           hover:bg-[length:100%_3px]
-                                           group-hover:bg-[length:100%_10px]
-                                           ark:from-purple-800 dark:to-purple-900">
-                                            {"Tiêu đề"}
-                                        </span>
-                                    </Link>
-                                </h2>
-
                                 <div className="hidden">
                                     <p className="mt-2 line-clamp-3 text-sm text-gray-500 dark:text-gray-400">
                                         <Link
